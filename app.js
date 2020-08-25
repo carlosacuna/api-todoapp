9c77d362fdb829e2b6c200d1d251c4ad392d38b9
Carlos Acuña <c.andres.a30@gmail.com>
2020-08-25 14:31:22 +0000

    Integrando cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+var cors = require('cors')
 const api = require('./routes')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+app.use(cors())
 
 app.use('/api', api)
 
