14ccb462b0c828c85147c2ab90bd8c5492074da9
Carlos Acuña <c.andres.a30@gmail.com>
2020-08-25 18:17:13 +0000

    Modificando modelo task  para relacionarlo con id usuario

--- models/task.js
+++ models/task.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const TaskSchea = Schema({
     title: String,
     description: String,
+    user_id: String, 
     state: {type: String, enum:['Pendiente','Completada']},
     created: { type: Date, default: Date.now() },
 })
